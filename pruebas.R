4c5fc5bd68aa0d790e59370b7298aa19160b699c
BrianPgz <brianpgz@ciencias.unam.mx>
2021-11-15 22:04:41 -0600

    Add pruebas file

---
+++ pruebas.R
@@ -0,0 +1,60 @@
+# Pruebas
+# read
+data=read.csv("monthly_beer.csv")
+
+data_ts=ts(data = data$Monthly.beer.production, start=c(1956,1),end = c(1995,8),frequency = 12)
+data_ts
+
+tsdisplay(data_ts)
+
+auto=auto.arima(data_ts)
+summary(auto)
+ggtsdisplay(auto$residuals)
+tsdiag(auto) #SE MUERE EL P-VALUE 
+checkresiduals(auto)
+
+
+
+
+sq_data= log(data_ts)
+t1=seq(1956,1995.59,by=1/12)
+bptest(sq_data ~ t1)
+tsdisplay(sq_data)
+mod=auto.arima(sq_data)
+summary(mod)
+
+t1 = 1: length(mod$residuals)
+bptest(mod$residuals ~ t1)
+ggtsdisplay(mod$residuals)
+tsdiag(mod, gof.lag = 50)
+checkresiduals(mod) #NO JALA EL P-VALUE PARA EL LOGARITMO 
+
+
+
+
+sqrt_m=sqrt(data_ts)
+ts.plot(sqrt_m)
+otro_mod=auto.arima(sqrt_m)
+summary(otro_mod)
+tsdiag(otro_mod, gof.lag = 50) #No jala la correlaion 
+
+
+diff_mod=(diff(data_ts))
+ts.plot(diff_mod)
+condiff=auto.arima(diff_mod)
+summary(condiff)
+tsdiag(condiff, gof.lag = 50)
+
+
+
+diff_mod=diff(sq_data)
+ts.plot(diff_mod)
+condiff=auto.arima(diff_mod)
+summary(condiff)
+tsdiag(condiff, gof.lag = 50)
+
+
+
+
+
+
